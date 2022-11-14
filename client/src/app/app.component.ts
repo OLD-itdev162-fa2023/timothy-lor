b70fe02de3613362c2aff57435d7a40acd1fc594
TimothyLor <lortg@gmatc.matc.edu>
2022-11-14 13:59:58 -0600

    Completed Activity 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,18 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'client';
+  title = 'BlogBox';
   posts: any;
 
-  constructor(private http: HttpClient){
+  constructor(){
 
   }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5064/api/posts').subscribe(
-      response => {this.posts = response;},
-      error => { console.log(error)}
-    );
+    
   }
 }
